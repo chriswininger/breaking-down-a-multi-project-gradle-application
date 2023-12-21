b0e1b96ed9b039755cb172f666d891970cd00345
Chris Wininger <cwininger@sonatype.com>
2023-12-21 16:40:31 -0500

    initial commit

---
+++ my-multi-module-test-v3/utilities/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+  kotlin("jvm") version "1.5.30"
+  `kotlin-dsl`
+  `java-library`
+}
+
+repositories {
+  // Use the plugin portal to apply community plugins in convention plugins.
+  gradlePluginPortal()
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.apache.commons:commons-text:1.10.0")
+  api(project(":list"))
+}
+
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(17))
+  }
+}
